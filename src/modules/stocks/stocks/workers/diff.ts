0515a71100ec20ea65931634fd8a92d6b0269bad
Andrian Todorov <andrian.todorov@gmail.com>
2022-08-12 16:20:14 +0300

    workers

---
+++ src/modules/stocks/stocks/workers/diff.ts
@@ -0,0 +1,47 @@
+import { SearchResult } from "../../datagenerator/interfaces/SearchResult";
+
+export default function calcDiff({ arr, min_tstamp, max_tstamp }) {
+    // Initialize Result
+    let maxDiff = -1;
+    let highestIndex = -1;
+    let highestIndexConfirmed = -1;
+    let lowestIndex = -1;
+
+
+
+
+    // Initialize max element from right side
+    let maxRight = arr[arr.length - 1].value;
+    highestIndex = arr.length - 1;
+
+
+    for (let i = arr.length - 2; i >= 0; i--) {
+        if (arr[i].tstamp > max_tstamp) {
+            continue;
+        }
+
+        if (arr[i].tstamp < min_tstamp) {
+            break;
+        }
+
+        if (arr[i].value >= maxRight) {
+            maxRight = arr[i].value;
+            highestIndex = i;
+        } else {
+            let diff = maxRight - arr[i].value;
+            if (diff > maxDiff) {
+                maxDiff = diff;
+                highestIndexConfirmed = highestIndex
+                lowestIndex = i;
+
+            }
+        }
+    }
+    return <SearchResult>{
+        buy_point: arr[lowestIndex].tstamp,
+        buy_point_price: arr[lowestIndex].value,
+        sell_point: arr[highestIndex].tstamp,
+        sell_point_price: arr[highestIndex].value
+    }
+
+}
