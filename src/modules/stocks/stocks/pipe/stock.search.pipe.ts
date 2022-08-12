ea6d826d2d8b6d0eb2e7eb6b38557ad820798752
Andrian Todorov <andrian.todorov@gmail.com>
2022-08-12 16:18:35 +0300

    stock search pipe - adds additional validation

---
+++ src/modules/stocks/stocks/pipe/stock.search.pipe.ts
@@ -0,0 +1,52 @@
+import { ArgumentMetadata, BadRequestException } from "@nestjs/common";
+import { Injectable, PipeTransform } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { ValidationException } from "src/core/validation/validation.filter";
+
+@Injectable()
+export class StockSearchPipe implements PipeTransform {
+
+    constructor(private configService: ConfigService) { }
+
+    transform(value: any, metadata: ArgumentMetadata) {
+        console.log('value', value)
+
+        let error = false;
+        let errorsResponse = {
+            start_time: [],
+            end_time: []
+        }
+
+        const configStartTime = this.configService.get<Number>('START_TIME');
+        const configEndTime = this.configService.get<Number>('END_TIME');
+
+
+        if (value.start_time < configStartTime) {
+            error = true;
+            errorsResponse.start_time.push(`Start time cannot be prior ${new Date(<number>configStartTime*1000).toISOString()}`)
+        }
+
+        if (value.end_time > configEndTime) {
+            error = true;
+            errorsResponse.end_time.push(`Start time cannot be past ${new Date(<number>configEndTime*1000).toISOString()}`)
+        }
+
+        if (value.start_time == value.end_time) {
+            error = true;
+            errorsResponse.start_time.push(`Start time cannot be the same as end time `)
+        }
+
+
+        if (value.start_time > value.end_time) {
+            error = true;
+            errorsResponse.start_time.push(`Start time cannot be past end time `)
+        }
+
+
+        if (error) {
+
+            throw new ValidationException({ errors: errorsResponse })
+        }
+        return value;
+    }
+}
