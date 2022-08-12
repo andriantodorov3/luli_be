6b9da12ba48daa370d57118dab864a9e02967db5
Andrian Todorov <andrian.todorov@gmail.com>
2022-08-12 15:57:53 +0300

    use workers instead of static payload for testing

--- src/modules/stocks/stocks/stocks.service.ts
+++ src/modules/stocks/stocks/stocks.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { DatageneratorService } from '../datagenerator/datagenerator.service';
+import { SearchResult } from '../datagenerator/interfaces/SearchResult';
+import { StocksSearchDto } from './dto/StocksSearchDto';
 
+import Piscina from 'piscina';
+import { resolve } from 'path';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
-export class StocksService {}
+export class StocksService {
+
+    private pool: Piscina;
+    constructor(private dataService: DatageneratorService
+    ) {
+
+        this.pool = new Piscina({
+            filename: resolve(__dirname, 'workers/index.js'),
+        });
+    }
+
+    async searchStocks(stocksSearchDto: StocksSearchDto) {
+
+        return await this.pool.run({ arr: this.dataService.data.items, min_tstamp: stocksSearchDto.start_time, max_tstamp: stocksSearchDto.end_time }, { name: 'calcDiff' });
+
+    }
+
+}
