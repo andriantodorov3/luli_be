f64f221983385a950974c457d69d3b8a9f061bb8
Andrian Todorov <andrian.todorov@gmail.com>
2022-08-17 19:17:20 +0300

    missing for testing

---
+++ src/modules/stocks/stocks/workers/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.calcDiff = void 0;
+var diff_1 = require("./diff");
+Object.defineProperty(exports, "calcDiff", { enumerable: true, get: function () { return __importDefault(diff_1).default; } });
+//# sourceMappingURL=index.js.map
